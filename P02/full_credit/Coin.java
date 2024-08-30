2ab4985afce8818c8409cb199efac1a94cf7cc52
katehdz <kkh1329@mavs.uta.edu>
2024-08-30 14:55:00 -0500

    update change

--- P02/full_credit/Coin.java
+++ P02/full_credit/Coin.java
@@ -1,19 +1,28 @@
 public class Coin {
+
+	//private fields
 	private Denomination denomination;
 	private int year;
 
+	//public constructor
 	public Coin(Denomination denomination, int year){
 		this.denomination =denomination;
 		this.year =year;
 	}
 
+	//public method 1
 	public double getValue() {
 		switch (denomination) {
 			case PENNY: return 0.01;
 			case NICKEL: return 0.05;
-			case DIME: return 0.10:
+			case DIME: return 0.10;
 			case QUARTER: return 0.25;
 			default: return 0.00;
 		}
+	}
+
+	//public method 2
+	public int getYear() {
+		return year;
 	}
 }
--- P02/full_credit/Purse.java
+++ P02/full_credit/Purse.java
@@ -1,1 +1,38 @@
 //Kaitlynn Hernandez CSE1325 P02 Full credit
+
+public class Purse {
+	public static void main (String[] args) {
+
+		//create coin array
+		Coin[] coins = {
+			new Coin(Denomination.PENNY, 1905 ),
+			new Coin(Denomination.NICKEL, 1981),
+			new Coin(Denomination.DIME, 1976),
+			new Coin(Denomination.DIME, 2020),
+			new Coin(Denomination.QUARTER, 1975)
+		};
+
+		double total=0.00;
+		int earliestCoin = Integer.MAX_VALUE;
+		int latestCoin = Integer.MIN_VALUE;
+
+		
+		for(Coin coin : coins){
+			//finding total value 
+			double value = coin.getValue();
+			int year = coin.getYear();
+			total += value;
+
+			//finding the earliest and latest coin in purse
+			if (year < earliestCoin){
+				earliestCoin = year;
+			}
+			if (year > latestCoin) {
+				latestCoin = year;
+			}
+		}
+
+		System.out.printf("You have $ %.2f in coins between %d and %d%n", total, earliestCoin, latestCoin);
+
+	}
+}
