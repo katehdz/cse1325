32754c157f5d588c8487ce0f55167a07f20af064
katehdz <kkh1329@mavs.uta.edu>
2024-09-01 19:13:25 -0500

    copy from bonus

---
+++ P02/extreme_bonus/Denomination.java
@@ -0,0 +1,20 @@
+public enum Denomination {
+	PENNY(0.01), NICKEL(0.05), DIME(0.10), QUARTER(0.25);
+
+	//private field 
+	private final double value;
+
+	//private constructor
+	Denomination(double value){
+		this.value = value;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	@Override
+	public String toString(){
+		return name().toLowerCase();
+	}
+}
