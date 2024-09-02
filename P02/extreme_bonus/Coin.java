394b1f8250f9620e2ab9d069a5deda4cff1d9c6e
katehdz <kkh1329@mavs.uta.edu>
2024-09-01 19:12:18 -0500

    final

--- P02/extreme_bonus/Coin.java
+++ P02/extreme_bonus/Coin.java
@@ -13,4 +13,30 @@ public class Coin {
 	public double getValue(){
 		return denomination.getValue();
 	}
+
+	//public method to get weight
+	public double getWeight(){
+		switch (denomination){
+			case PENNY:
+				if (year <1983) return 3.110;
+				else return 2.500;
+			case NICKEL:
+				return 5.000
+			case DIME:
+				if (year <1965) return 2.500;
+				else return 2.268;
+			case QUARTER:
+				if (year <1965) return 6.250;
+				else return 5.670;
+		}
+	}
+	//public method to get year
+	public int getYear(){
+		return year;
+	}
+
+	@Override
+	public String toString(){
+		return year + " " + denomination.toString();
+	}
 }
