c4fdb2001e8c04dd5f1770325da5a44f082db670
katehdz <kkh1329@mavs.uta.edu>
2024-09-09 18:53:39 -0500

    media

---
+++ P03/Media.java
@@ -0,0 +1,16 @@
+public class Media {
+
+	private String title;
+	private String url;
+
+	//constructor
+	public Media(String title, String url){
+		this.title= title;
+		this.url=url;
+	}
+
+	@Override
+	public String toString(){
+		return title + " (" + url + ")";
+	}
+}
