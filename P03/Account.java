480c11bb11ea86980856626bc62f8ccaeee0b33c
katehdz <kkh1329@mavs.uta.edu>
2024-09-09 19:00:16 -0500

    account

---
+++ P03/Account.java
@@ -0,0 +1,16 @@
+public class Account {
+	private int accountNumber;
+	private static int nextaccountNumber=1;
+
+	public Account(){
+		this.accountNumber = nextaccountNumber++;
+	}
+
+	public int getAccountNumber (){
+		return accountNumber;
+	}
+
+	public String play(Media media){
+		return "Playing " + media.toString();
+	}
+}
