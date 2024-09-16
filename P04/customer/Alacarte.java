d90599a7ea3072f4d4f989cc7b87adc6827a4b41
katehdz <kkh1329@mavs.uta.edu>
2024-09-16 11:14:16 -0500

    update test

--- P04/customer/Alacarte.java
+++ P04/customer/Alacarte.java
@@ -2,11 +2,14 @@ package customer;
 
 import product.Media;
 
-public class Alacarte extends Accounts {
-	private int pointsRemaining;
+public class Alacarte extends Account {
+	private int pointsRemaining=0;
+
 
 	public void buyPoints(int points){
+		if (points > 0){
 		pointsRemaining += points;
+		}
 	}
 
 	public int getPointsRemaining(){
@@ -15,13 +18,12 @@ public class Alacarte extends Accounts {
 
 	@Override
 	public String play(Media media){
-		int mediaPoints = media.getPoints():
-
+		int mediaPoints = media.getPoints();
 		if (pointsRemaining >= mediaPoints){
 			pointsRemaining-= mediaPoints;
 			return "Playing " + media.toString();
 		} else {
-			return "Buy more points: Requires " + mediaPoints + " points, you have " + pointsRemaining;
+			return "Buy more points: Requires " + mediaPoints + " points, you have " + pointsRemaining + " points";
 		}
 	}
 }
--- P04/test/TestAccount.java
+++ P04/test/TestAccount.java
@@ -1,10 +1,12 @@
 package test;
 
 import customer.Account;
+import customer.Alacarte;
+import customer.Unlimited;
+import product.Media;
 
 public class TestAccount {
 	public static void main(String[] args){
-
 		int accountToTest=2;
 
 		for (int i=1; i<= accountToTest; i++){
@@ -15,5 +17,33 @@ public class TestAccount {
 			System.exit(1);
 			}
 		}
+
+		// Check Unlimited and Alacarte
+        Unlimited unlimitedAccount = new Unlimited();
+        Alacarte alacarteAccount = new Alacarte();
+        Media media = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0", 10);
+
+
+        // Test buying points
+        alacarteAccount.buyPoints(15); // Adding points
+        if (alacarteAccount.getPointsRemaining() != 15) {
+            System.out.println("Fail:\nExpected points remaining: 15\nActual points remaining: " + alacarteAccount.getPointsRemaining());
+            System.exit(1);
+        }
+
+        // Test playing media with enough points
+        String result = alacarteAccount.play(media);
+        if (!result.equals("Playing The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0, 10 points)")) {
+            System.out.println("Fail:\nExpected: Playing The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0, 10 points)\nActual: " + result);
+            System.exit(1);
+        }
+
+        // Manually reset points to 0
+        alacarteAccount = new Alacarte(); // Creating a new instance to ensure pointsRemaining is 0
+        result = alacarteAccount.play(media);
+        if (!result.equals("Buy more points: Requires 10 points, you have 0 points")) {
+            System.out.println("Fail:\nExpected: Buy more points: Requires 10 points, you have 0 points\nActual: " + result);
+            System.exit(1);
+        }
 	}
 }
