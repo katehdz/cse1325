3c9efb6e5c9ade98abe926dc04efa1682df2fc70
katehdz <kkh1329@mavs.uta.edu>
2024-10-07 21:28:45 -0500

    student

--- P06/customer/Student.java
+++ P06/customer/Student.java
@@ -1,6 +1,9 @@
 package customer;
 
 import product.Media;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class Student {
 	private String name;
@@ -22,9 +25,37 @@ public class Student {
         }
     } else {
         throw new IllegalArgumentException("Non-UTA email address: " + email);
-    }
-}
+    	}
+	}
+	
+	public Student (BufferedReader br) throws IOException {
+		this.name= br.readLine();
+		this.id = Integer.parseInt(br.readLine());
+		this.email = br.readLine();
 
+		String accountType = br.readLine();
+		if (accountType.equals("alacarte")) {
+            this.account = new Alacarte(br);  
+        } else if (accountType.equals("unlimited")) {
+            this.account = new Unlimited(br);  
+        } else {
+            throw new IOException("Unknown account type: " + accountType);
+        }
+	}
+
+	public void save(BufferedWriter bw) throws IOException {
+        bw.write(name + "\n"); 
+        bw.write(Integer.toString(id) + "\n");  
+        bw.write(email + "\n");  
+
+        // Write account type (either "alacarte" or "unlimited")
+        if (account instanceof Alacarte) {
+            bw.write("alacarte\n");
+        } else if (account instanceof Unlimited) {
+            bw.write("unlimited\n");
+        }
+        account.save(bw);  
+    }
 
 	public String requestMedia(Media media){
 		return account.play(media);
