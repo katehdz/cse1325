a273a14463986abc980189aa9b5c150561b6c2a7
katehdz <kkh1329@mavs.uta.edu>
2024-11-01 16:19:47 -0500

    changed verification

--- P09/full_credit/clock.cpp
+++ P09/full_credit/clock.cpp
@@ -1,7 +1,13 @@
 #include "clock.h"
 
-Clock::Clock(int hours, int minutes, int seconds) : _hours(hours), _minutes(minutes), _seconds(seconds){
-	if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || seconds < 0 || seconds >59){
-		throw std::out_of_range("Invalid time values");
-	}
+Clock::Clock(int hours, int minutes, int seconds) : _hours(hours), _minutes(minutes), _seconds(seconds) {
+    if (hours < 0 || hours > 23) {
+        throw std::out_of_range("Invalid hour value: " + std::to_string(hours));
+    }
+    if (minutes < 0 || minutes > 59) {
+        throw std::out_of_range("Invalid minute value: " + std::to_string(minutes));
+    }
+    if (seconds < 0 || seconds > 59) {
+        throw std::out_of_range("Invalid second value: " + std::to_string(seconds));
+    }
 }
