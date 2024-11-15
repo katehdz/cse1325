b1e32be5fe403264595192cc7d156898ff6cca50
katehdz <kkh1329@mavs.uta.edu>
2024-11-15 14:35:07 -0600

    temps

--- P11/full_credit/date.h
+++ P11/full_credit/date.h
@@ -12,11 +12,11 @@ private:
 
 public: 
 
-	Date(int year = 1970, int month =1, int day =1)
+	Date(int year = 1970, int month =1, int day =1);
 
 	auto operator<=>(const Date&) const = default;
 
-	friend std::ostream& operator<<(std::ostream& ost, const Date& date) 
+	friend std::ostream& operator<<(std::ostream& ost, const Date& date);
 };
 
 #endif
